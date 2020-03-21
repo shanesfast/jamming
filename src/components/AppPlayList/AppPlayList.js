21bf2fb811a5d8408f16ecb4b549d05a2a1b96e2
Shane Quick <shanesfast@gmail.com>
2020-03-20 20:49:20 -0500

    Add env var, add refs

--- src/components/AppPlayList/AppPlayList.js
+++ src/components/AppPlayList/AppPlayList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SearchResults } from '../SearchResults/SearchResults.js';
-import { PlayList } from '../PlayList/PlayList.js';
-import { EditBox } from '../EditBox/EditBox.js';
+import PlayList from '../PlayList/PlayList.js';
+import EditBox from '../EditBox/EditBox.js';
 import './AppPlayList.css';
 
 export const AppPlayList = (props) => {
