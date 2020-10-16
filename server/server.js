015e12b8504b2b1d852e81ec3dfd21fa8e42cf7b
Shane Quick <shanesfast@gmail.com>
2020-10-16 13:20:27 -0500

    Update build folder path

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ mongoose.connect(db, {
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('/build'));
+  app.use(express.static('../build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
